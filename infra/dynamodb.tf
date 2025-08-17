34b08fee8611840e2a4623336539fd3866c32bc0
CristinaPineda <cristinadaspineda@yahoo.com.br>
2025-08-17 18:51:53 -0300

    :zap: inicia criação tabelas admissao e pertences

---
+++ infra/dynamodb.tf
@@ -0,0 +1,45 @@
+# Tabela 1: Ficha de Admissão
+resource "aws_dynamodb_table" "ficha_admissao" {
+  name           = "FichaAdmissao"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "cpf"
+  range_key      = "nome"
+
+  attribute {
+    name = "cpf"
+    type = "S"
+  }
+
+  attribute {
+    name = "nome"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "FichaAdmissao"
+    Environment = var.environment
+  }
+}
+
+# Tabela 2: Tabela de Pertences
+resource "aws_dynamodb_table" "pertences" {
+  name           = "Pertences"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "cpf"
+  range_key      = "nome"
+
+  attribute {
+    name = "cpf"
+    type = "S"
+  }
+
+  attribute {
+    name = "nome"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Pertences"
+    Environment = var.environment
+  }
+}
